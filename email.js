a71555c96c5de59c11512df2ede16359b85c1adc
Sam James <Sam.James@softwire.com>
2022-01-19 12:33:37 +0000

    testing domain regex

--- email.js
+++ email.js
@@ -2,19 +2,23 @@ const fs = require('fs');
 
 const file = fs.readFileSync('test.txt', {encoding: 'utf8'});
 
-console.log(file.length);
+// console.log(file.length);
 
-let counter = 0;
-const substring = "@softwire.com";
+// let counter = 0;
+// const substring = "@softwire.com";
 
-for(let i = 0; i < file.length; i++) {
-    if(file.substring(i, i+13) === substring) {
-        counter ++
-    }
-}
+// for(let i = 0; i < file.length; i++) {
+//     if(file.substring(i, i+13) === substring) {
+//         counter ++
+//     }
+// }
 
-console.log(counter);
+// console.log(counter);
 
+// REGEX solution
 
+const domain = new RegExp(/\w+@softwire\.com\W/);
+const test = new RegExp(/\w+@\w+\.\w+/g);
 
+console.log(file.match(domain));
 
