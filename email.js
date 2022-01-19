90d52a125819e1920bfb35ac4728241df55e3bf0
Chiara Dinan <Chiara.Dinan@softwire.com>
2022-01-19 11:20:54 +0000

    implemented a counter for the substring '@softwire.com using a for loop and the substring method'

--- email.js
+++ email.js
@@ -1,2 +1,20 @@
 const fs = require('fs');
-const file = fs.readFileSync('test.txt' {});
+
+const file = fs.readFileSync('test.txt', {encoding: 'utf8'});
+
+console.log(file.length);
+
+let counter = 0;
+const substring = "@softwire.com";
+
+for(let i = 0; i < file.length; i++) {
+    if(file.substring(i, i+13) === substring) {
+        counter ++
+    }
+}
+
+console.log(counter);
+
+
+
+
